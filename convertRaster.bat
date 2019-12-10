4c90404620f72bde9a7aad78973159b5a80a52c2
bendohan <55117201+bendohan@users.noreply.github.com>
2019-12-10 15:52:03 -0500

    Update convertRaster.bat

--- convertRaster.bat
+++ convertRaster.bat
@@ -4,8 +4,6 @@ K:\gg323\raster2pgsql\raster2pgsql -s 4326 -d UNEP_GRID\dr_physexp.tif public.dr
 
 K:\gg323\raster2pgsql\raster2pgsql -s 4326 -d UNEP_GRID\fl_physexp.tif public.floodexposure  > floodexposure.sql
 
-:: K:\gg323\raster2pgsql\raster2pgsql UNEP_GRID\dr_physexp.tif public.floodrisk  > floodrisk.sql
-
 pause
 
 :: K:\gg323\raster2pgsql\raster2pgsql is the location of the raster2pgsql program on Splinter. If you save the program somewhere else, change this address It's normally installed in the program files\postgresql\12\bin folder of your postgresql server install. The number 12 will change with each postgresql version. PostGIS must be installed to the server, with raster support enabled.
